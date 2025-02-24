20c5b49406228969221b4062c54641339711feb6
Ohyoung Song <songohyoung@gmail.com>
2025-02-24 23:29:36 +0900

    .env 을 숨겼다. route.ts의 merge conflict를 해결하였다.

--- src/app/api/send-message/route.ts
+++ src/app/api/send-message/route.ts
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import webPush, { PushSubscription, sendNotification } from "web-push";
-
-export async function POST(req: Request) {
-
-try {
-  const { pushSubscription, title, message } = await req.json();
-
-  //console.log("send-message.POST---pushSubscription, title, message: ", pushSubscription, title, message);
-
-  if (!pushSubscription || !title || !message) {
-      return new Response(JSON.stringify({ error: 'Invalid request data' }), { status: 400 });
-  }
-
-  const subscription = pushSubscription as PushSubscription;
-  const payload = JSON.stringify({ title, message });  
-
-  if (
-    !process.env.VAPID_SUBJECT ||
-    !process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY ||
-=======
 import webPush, { PushSubscription } from "web-push";
 
 export async function POST(req: Request) {
@@ -29,7 +8,6 @@ export async function POST(req: Request) {
   if (
     !process.env.VAPID_SUBJECT ||
     !process.env.VAPID_PUBLIC_KEY ||
->>>>>>> origin/main
     !process.env.VAPID_PRIVATE_KEY
   ) {
     console.error("VAPID key 정보가 없습니다.");
@@ -39,26 +17,12 @@ export async function POST(req: Request) {
   const options = {
     vapidDetails: {
       subject: process.env.VAPID_SUBJECT,
-<<<<<<< HEAD
-      publicKey: process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY,
-=======
       publicKey: process.env.VAPID_PUBLIC_KEY,
->>>>>>> origin/main
       privateKey: process.env.VAPID_PRIVATE_KEY,
     },
     TTL: 60,
   };
 
-<<<<<<< HEAD
-  // sendNotification 함수 사용
-  await sendNotification(subscription, payload, options);
-//   
-  return new Response(JSON.stringify({ success: true }), { status: 201 });
-} catch (error) {
-  console.error('Error sending notification:', error);
-  return new Response(JSON.stringify({ error: 'Failed to send notification' }), { status: 500 });
-}
-=======
   try {
     const response = await webPush.sendNotification(
       subscription,
@@ -70,5 +34,4 @@ export async function POST(req: Request) {
     console.error("notification error", error);
     return Response.error();
   }
->>>>>>> origin/main
 }
